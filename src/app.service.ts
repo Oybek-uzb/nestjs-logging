b70e25d8e1e66947e4e329f1a20c80ed6826954e
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-29 06:15:44 +0500

    start working

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger(AppService.name) // uses this name when logs something
+  }
+
   getHello(): string {
+    this.logger.debug('GET method') // for debug level
     return 'Hello World!';
+  }
+
+  getHelloFromPost(): string {
+    return 'Hello from post request!'
   }
 }
